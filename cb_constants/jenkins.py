13db61fb57dec4b195b51d8e258c92a0664eb333
Ashwin <ashwintrojan+github@gmail.com>
2024-12-16 14:54:30 +0530

    Appending URL to jenkins url variables

--- cb_constants/jenkins.py
+++ cb_constants/jenkins.py
@@ -1,5 +1,5 @@
 class JenkinsConstants:
-    OLD_JENKINS = "http://172.23.109.231"
-    NEW_JENKINS = "http://172.23.121.80"
+    OLD_JENKINS_URL = "http://172.23.109.231"
+    NEW_JENKINS_URL = "http://172.23.121.80"
 
     TOKEN = "extended_sanity"
