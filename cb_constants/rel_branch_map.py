0b26a7177c108447cc7b2241add2c9b6de555e17
Ashwin <ashwintrojan+github@gmail.com>
2024-12-16 14:00:23 +0530

    Adding server:release-branch map and Jenkins constants file

---
+++ cb_constants/rel_branch_map.py
@@ -0,0 +1,16 @@
+CB_VERSION_NAME = {"0.0": "master",
+                   "6.0": "alice",
+                   "6.5": "mad-hatter", "6.6": "mad-hatter",
+                   "7.0": "cheshire-cat",
+                   "7.1": "neo", "7.2": "neo",
+                   "7.5": "elixir",
+                   "7.6": "trinity",
+                   "7.7": "cypher",
+                   "8.0": "morpheus"}
+
+
+COLUMNAR_VERSION_NAME = {
+    "1.0.0": ["1.0.0"],
+    "goldfish": ["1.0.1", "1.0.2", "1.0.3", "1.0.4", "1.0.5"],
+    "ionic": ["1.1.0"],
+}
